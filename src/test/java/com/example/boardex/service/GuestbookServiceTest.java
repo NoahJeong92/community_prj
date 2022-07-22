0ef36d4feb7c9270ba50aa5594f1fe5da51d2dd9
NoahJeong92 <skye@Skyes-MacBook-Pro-2.local>
2022-07-22 11:44:05 +0900

    hook test

--- src/test/java/com/example/boardex/service/GuestbookServiceTest.java
+++ src/test/java/com/example/boardex/service/GuestbookServiceTest.java
@@ -1,46 +1,46 @@
-package com.example.boardex.service;
-
-import com.example.boardex.dto.GuestbookDTO;
-import com.example.boardex.dto.PageRequestDTO;
-import com.example.boardex.dto.PageResultDTO;
-import com.example.boardex.domains.entity.Guestbook;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-//@SpringBootTest
-class GuestbookServiceTest {
-
-    @Autowired
-    GuestbookService service;
-
-    @Test
-    public  void testRegister(){
-        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
-                .title("Sample Title....")
-                .content("Contest.....")
-                .writer("정태환")
-                .build();
-        System.out.println(service.register(guestbookDTO));
-    }
-
-    @Test
-    public void testList(){
-
-        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
-
-        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
-
-//        System.out.println("PREV: "+resultDTO.isPrev());
-//        System.out.println("NEXT: "+resultDTO.isNext());
-//        System.out.println("TOTAL: " + resultDTO.getTotalPage());
-
-        System.out.println("-------------------------------------");
-        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
-            System.out.println(guestbookDTO);
-        }
-
-//        System.out.println("========================================");
-//        resultDTO.getPageList().forEach(i -> System.out.println(i));
-    }
-}
+//package com.example.boardex.service;
+//
+//import com.example.boardex.dto.GuestbookDTO;
+//import com.example.boardex.dto.PageRequestDTO;
+//import com.example.boardex.dto.PageResultDTO;
+//import com.example.boardex.domains.entity.Guestbook;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+////@SpringBootTest
+//class GuestbookServiceTest {
+//
+//    @Autowired
+//    GuestbookService service;
+//
+//    @Test
+//    public  void testRegister(){
+//        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+//                .title("Sample Title....")
+//                .content("Contest.....")
+//                .writer("정태환")
+//                .build();
+//        System.out.println(service.register(guestbookDTO));
+//    }
+//
+//    @Test
+//    public void testList(){
+//
+//        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+//
+//        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+//
+////        System.out.println("PREV: "+resultDTO.isPrev());
+////        System.out.println("NEXT: "+resultDTO.isNext());
+////        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+//
+//        System.out.println("-------------------------------------");
+//        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+//            System.out.println(guestbookDTO);
+//        }
+//
+////        System.out.println("========================================");
+////        resultDTO.getPageList().forEach(i -> System.out.println(i));
+//    }
+//}
